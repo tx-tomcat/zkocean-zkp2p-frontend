c591c5ce6e4a71c1d18e5091eee106578b6cc48b
dzungdinh94 <dungnd8594@gmail.com>
2024-05-31 04:03:54 +0700

    init

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const ZKRAMP_ID = '0x3f45b7b243fcf99e3e812f7ad79b9f8a36135073348fc1f5d8023d8f9027c682'
-export const baseApiUrl = 'http://154.26.132.141/api'
+export const baseApiUrl = 'https://alphabe.net/api'
